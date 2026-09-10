65af00d0d669d45b7b0abca485b96b2dcdaa9827
Bernardo <bernardobregeron@gmail.com>
2026-09-10 20:24:30 -0300

    added new enpoint

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
+data = {'integrantes':[
+
+{'nome':'Bernardo Bregeron'},
+{'nome':'Lucas Ricci'},
+{'nome':'Mateus Carpenter'}
+
+]}
+
 app.get('/', (req, res) => {
   res.sendFile('views/index.html', { root: __dirname });
 });
+
+
+app.get('/integrantes', (req, res) => {
+
+res.json(data)
+
+})
